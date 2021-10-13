fd98ede2a374180e6358c5883146439fce1dd99e
pawelzsbem <pawel.marcin@zsbem.edu.pl>
2021-10-13 12:45:15 +0000

    moving snake's head

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -13,9 +13,31 @@ class Grid extends React.Component{
 
 
         let grid = Array(this.props.size[0]).fill(Array(this.props.size[1]).fill(null));
-        return ( <div className="grid"> {grid.map((gridX, indexX)=>
+        return (<> <div className="grid"> {grid.map((gridX, indexX)=>
             {return ( <div className="snake-row"> { gridX.map((valueY, indexY) => {
-                 return <Tile X={indexX} Y={indexY} snake={this.props.snake} food={this.props.food}></Tile>} )  } </div>);  }  )} </div>);
+                 return <Tile X={indexX} Y={indexY} snake={this.props.snake} food={this.props.food}></Tile>} )  } </div>);  }  )} </div> 
+                 <div className="button-area"> 
+
+                 <table>
+                     <tr><td></td><td><button className="dir" onClick={()=>this.props.onClick("up")}>up</button></td><td></td></tr>
+                     <tr><td><button className="dir"  onClick={()=>this.props.onClick("left") }>left</button></td><td></td><td><button className="dir"onClick={()=>this.props.onClick("right") } >right</button></td></tr>
+                     <tr><td></td><td><button className="dir"onClick={()=>this.props.onClick("down") } >down</button></td><td></td></tr>
+                     
+
+                </table>
+
+                <p>
+                    Current direction: {this.props.dir}
+                </p>
+
+                <p>
+                    Time elapsed: {this.props.time}
+                </p>
+
+
+
+                 
+                 </div></>);
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Grid from './components/Grid.js';
 import "./index.css";
@@ -16,17 +16,68 @@ class Game extends React.Component{
         this.state = {
             gridsize: [gridWidth, gridHeight], 
             food: [3, 6], 
-            snake: [1, 2]
+            snake: [1, 2], 
+            direction: "left", 
+            time: 0
 
         }
+        this.onClick = this.onClick.bind(this);
+        this.timeElaps = this.timeElaps.bind(this);
+    }
+
+    timeElaps(){
+        //this.setState({time: this.state.time + 1});
+
+        let newSnake = [];
+        let tmp = 0;
+        const gridSizX = this.state.gridsize[0];
+        const gridSizY = this.state.gridsize[1];
+
+
+        switch(this.state.direction){           
+            
+            case "up":
+                tmp = this.state.snake[0] - 1 < 0? this.state.gridsize[0] - 1 :this.state.snake[0] - 1;
+                newSnake = [tmp, this.state.snake[1] ];
+                break;
+            case "down":
+                tmp = (this.state.snake[0] + 1) % this.state.gridsize[0];
+                newSnake = [tmp, this.state.snake[1] ];
+                break;
+            case "left":
+                tmp = this.state.snake[1] - 1 < 0? this.state.gridsize[1] - 1 :this.state.snake[1] - 1;
+                newSnake = [this.state.snake[0], tmp ];
+                break;
+            case "right":
+                tmp = (this.state.snake[1] + 1) % this.state.gridsize[0];
+                newSnake = [this.state.snake[0], tmp ];
+                break;
+
+            default:
+                newSnake = [parseInt(this.state.gridsize[0]/2), parseInt(this.state.gridsize[1]/2)];
+        }
+
+        this.setState({snake: newSnake, time: this.state.time + 1});
+
+
+    }
+
+
+    componentDidMount(){
+        this.setState({snake: [parseInt(this.state.gridsize[0]/2), parseInt(this.state.gridsize[1]/2)]});
+        setInterval(this.timeElaps, 500);
+    }
+
+    onClick(nxtDir){
+        this.setState({direction: nxtDir});
+
     }
 
     render(){
         return <>
-            <Grid size={this.state.gridsize} food={this.state.food} snake={this.state.snake} />
+            <Grid size={this.state.gridsize} time={this.state.time} food={this.state.food} snake={this.state.snake} dir={this.state.direction} onClick={this.onClick} />
         </>
     }
 }
-
 
 ReactDOM.render(<Game />, document.getElementById("root"));
